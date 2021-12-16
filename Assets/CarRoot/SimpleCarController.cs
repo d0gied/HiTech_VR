1c612679edea6871295a5830310ea653b751c578
germannu <57252247+germannu@users.noreply.github.com>
2021-12-16 15:38:54 +1000

    truck

--- Assets/CarRoot/SimpleCarController.cs
+++ Assets/CarRoot/SimpleCarController.cs
@@ -1,16 +1,35 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
     
 public class SimpleCarController : MonoBehaviour {
     public List<AxleInfo> axleInfos; 
     public float maxMotorTorque; 
     public float maxSteeringAngle;
+    public float rspeed = 0f;
+    public float lspeed = 0f;
     public float speed = 0f;
     public float steeringAngle = 0f;
-        
+    public Transform steeringWheel;
+    public XRController RightController;
+    public XRController LeftController;
+
+    public void Start()
+    {
+    }
+
     public void FixedUpdate()
     {
+        steeringAngle = steeringWheel.localRotation.eulerAngles.x;
+        if (steeringAngle > 90)
+            steeringAngle -= 360;
+        steeringAngle /= 90;
+        RightController.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out rspeed);
+        LeftController.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out lspeed);
+        speed = rspeed - lspeed;
         float motor = maxMotorTorque * speed;
         float steering = maxSteeringAngle * steeringAngle;
             
